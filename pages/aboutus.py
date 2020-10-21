38523196c31a027614909b2b4cf244525d265c6b
jproctor-rebecca <jproctor.m.ed.tn@gmail.com>
2020-10-21 13:26:38 -0500

    update about us page

--- pages/aboutus.py
+++ pages/aboutus.py
@@ -5,4 +5,43 @@ website or blog space
 
 RJProctor
 '''
+# Imports from 3rd party libraries
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+# Imports from this application
+from app import app
+
+# 1 column layout
+# https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
+column1 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+        
+            ## The Team: 
+            Select a link to learn more about each of our 
+            team members.    
+
+
+
+            """
+
+            html.Label(['Debbie Cohen ', html.A('link', href='/link-location')])
+            html.Label(['Moe Fa ', html.A('link', href='/link-location')])
+            html.Label(['Eduardo Padilla ', html.A('link', href='/link-location')])
+            html.Label(['Jeannine Proctor ', html.A('link', href='/proctor-rebecca.github.io')])
+        ),
+
+    ],
+)
+
+layout = dbc.Row([column1])
+
+
+
+
 
