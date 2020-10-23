7aeec858d114b682c27824e78cb3b3eed4d589eb
jproctor-rebecca <jproctor.m.ed.tn@gmail.com>
2020-10-23 09:36:18 -0500

    update insights

--- pages/insights.py
+++ pages/insights.py
@@ -21,10 +21,25 @@ column1 = dbc.Col(
             """
             ## Insights from the Team: 
             # 
-            For performance metrics, linear regression and LassoCV performed nearly equivalently, both returning MAEs between 56 and 58 for both training and validation data sets. 
+            To determine which model would perform best, we test several with our data prior to applying one in the application.
+            We test the mathematical algorithms:
+            Linear Regression, LassoCV, Decision Tree Regressor, XGBoost, and LightGBM Regressor (LGBMR). 
+            For performance metrics, linear regression and LassoCV perform nearly equivalently, both returning MAEs between 56 and 58 for both training and validation data sets. 
+            LightGBM is a gradient boosting framework that uses a tree-based algorithm. 
+            The LGBMR is most similar to the popular ML algorithm that is XGBoost. 
+            The biggest difference being that while XGBoost splits the tree nodes one level at a time, the LGBMR splits one node at a time. 
+            LGBM also boasts more parameters, much faster speed, all with comparable accuracy metrics.
 
-            The plot below shows the top 30% of the most influential features in each of these models’ fit, and their coefficients. The linear regression model showed city, cancellation policy and room type as being the most influential on the rental price. The LassoCV model, after performing its own feature selection process, returned a room type of “entire apartment” as the most influential factor on the price, followed by the number of people accommodated and the number of bedrooms and bathrooms, and being in the city of San Francisco.
+            As we look at feature importance we discover that some are more useful than others. 
+            Feature importance shows the value of each feature as it relates to the optimal price for a given Airbnb property. Some of the new created features that were not so evident in the base model include features such as: strict and super_strict_60. This makes sense because it shows that the cancellation policy is very telling when it come to predicting an Airbnb price. A super strict 60 day policy on an Airbnb would certainly imply a higher demand than properties with a much more lenient cancellation policy.
+            The plot aside shows the top 30% of the most influential features in each of these models’ fit, and their coefficients. 
+            We see cancellation policy and room type as being very influential on the rental price. 
+            This makes sense because it shows that the cancellation policy is very telling when it come to predicting an Airbnb price. 
+            A super strict 60 day policy on an Airbnb would certainly imply a higher demand than properties with a much more lenient cancellation policy.
+
+            
     
+            
             """
         ),
 
